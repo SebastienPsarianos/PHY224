cacfad34db9b9a7a3f205d610f117025ff77a245
Sebastien Psarianos <sebastienpsarianos@Sebastiens-MacBook-Pro.local>
2022-11-06 18:35:26 -0500

    polarizationLabStuff

--- polarizationLab/python/main.py
+++ polarizationLab/python/main.py
@@ -1,13 +1,27 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
+from scipy.constants import pi
 import os
 
 directory = os.path.dirname(os.path.realpath(__file__))
 
-experimentOneFile = open('experimentOne.txt', 'w')
-experimentTwoFile = open('experimentTwo.txt', 'w')
 
-# expOneAngles, expOneAngles = np.loadtxt(f"{directory}/rawData/experimentOne.txt", delimiter=',', unpack=True)
-# expTwoAngles, expTwoAngles = np.loadtxt(f"{directory}/rawData/experimentTwo.txt", delimiter=',', unpack=True)
-# expThreeAngles, expThreeAngles = np.loadtxt(f"{directory}/rawData/experimentThree.txt", delimiter=',', unpack=True)
+def cosineModel(x, amplitude, omega):
+    return amplitude * np.cos(omega, x)
 
+
+test = np.loadtxt(f"{directory}/rawData/experimentThree.txt", delimiter="\t")
+
+angles, intensity = np.loadtxt(
+    f"{directory}/rawData/experimentThree.txt", delimiter="\t", unpack=True
+)
+
+angles = angles * pi / 180
+
+test = angles[:]
+test2 = intensity[:]
+test3 = curve_fit(cosineModel, test, test2)
+
+plt.errorbar(angles, intensity)
+plt.show()
