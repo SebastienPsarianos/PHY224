c40581482fdfe749603a63f5f089723a8be0452e
Sebastien Psarianos <sebastienpsarianos@Sebastiens-MacBook-Pro.local>
2022-11-28 16:00:37 -0500

    consolidated stuff

--- millikanLab/python/main.py
+++ millikanLab/python/main.py
@@ -1,28 +1,202 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+from statistics import mean
 import os
-import regex
 
 directory = os.path.dirname(os.path.realpath(__file__))
 
+# Defining the constant
+constant = (18 * (6 / 1000) * np.pi * 0.00001827 ** (3 / 2)) / (
+    np.sqrt(2) * np.sqrt(9.8) * np.sqrt(875.3 - 1.204)
+)
 
-def sortByInt(fileName):
-    return int(regex.match(r"\d*", fileName)[0])
+#############################
+# Uncertainty / Propagation #
+#############################
+
+# Uncertainty in voltage readings and error propagation
+def uncertaintyVoltage(reading, ran):
+    return 0.006 * reading + 0.001 * ran
 
 
-# Replace this with the directory that the files are in
+def uncertaintyChargeMethodOne(
+    velocityOne, voltageOne, uncertaintyOne, uncertaintyTwo
+):
+    return np.sqrt(
+        (3 * constant / 2 * np.sqrt(abs(velocityOne)) / voltageOne) ** 2
+        * uncertaintyOne ** 2
+        + (-1 * constant * abs(velocityOne) ** (3 / 2) / voltageOne ** 2) ** 2
+        * uncertaintyTwo ** 2
+    )
+
+
+def uncertaintyChargeMethodTwo(
+    velocityOne,
+    velocityTwo,
+    voltageTwo,
+    uncertaintyOne,
+    uncertaintyTwo,
+    uncertaintyThree,
+):
+    return np.sqrt(
+        (
+            (3 * constant / 2 * np.sqrt(abs(velocityOne)))
+            + (
+                constant
+                / 2
+                * velocityTwo
+                / voltageTwo
+                / np.sqrt(abs(velocityOne))
+            )
+        )
+        ** 2
+        * uncertaintyOne ** 2
+        + (constant * np.sqrt(abs(velocityOne)) / voltageTwo) ** 2
+        * uncertaintyTwo ** 2
+        + (
+            (-1 * constant * abs(velocityOne) ** (3 / 2) / voltageTwo ** 2)
+            - (
+                constant
+                * velocityTwo
+                * np.sqrt(abs(velocityOne))
+                / voltageTwo ** 2
+            )
+        )
+        ** 2
+        * uncertaintyThree ** 2
+    )
+
+
+#############################
+# Charge Calculation Models #
+#############################
+def chargeMethodOne(velocityOne, voltageOne):
+    return constant * velocityOne ** (3 / 2) / voltageOne
+
+
+# Method 2
+def chargeMethodTwo(velocityOne, velocityTwo, voltageTwo):
+    return (
+        constant
+        * (abs(velocityOne) + velocityTwo)
+        * abs(velocityOne) ** (1 / 2)
+        / voltageTwo
+    )
+
+
+# General Linear Model
+def linear(time, velocity, intercept):
+    return time * velocity + intercept
+
+
 sampleDataFileNames = os.listdir(f"{directory}/rawData/")
-sampleDataFileNames.sort(key=sortByInt)
+sampleDataFileNames.sort(key=lambda f: int("".join(filter(str.isdigit, f))))
 
 samples = []
 for fileName in sampleDataFileNames:
-    fallingPositions = []
-    risingPositions = []
+    fallingPositions = np.array([])
+    risingPositions = np.array([])
     test = open(f"{directory}/rawData/{fileName}")
     lines = test.readlines()
     for line in lines:
-        fallingPosition, risingPosition = line.strip("\n").split(",")
-        if fallingPosition != "":
-            fallingPositions.append(int(fallingPosition))
+        risingPosition, fallingPosition = line.strip("\n").split(",")
         if risingPosition != "":
-            risingPositions.append(int(risingPosition))
+            risingPositions = np.append(
+                risingPositions, int(risingPosition) / 540000
+            )
+        if fallingPosition != "":
+            fallingPositions = np.append(
+                fallingPositions, int(fallingPosition) / 540000
+            )
     test.close()
-    samples.append((fallingPositions, risingPositions))
+    samples.append((risingPositions, fallingPositions))
+
+stoppingVoltage, voltageUp = np.loadtxt(
+    f"{directory}/voltages.csv", delimiter=",", unpack=True
+)
+stoppingVoltageUncertainty = uncertaintyVoltage(stoppingVoltage, 1100)
+voltageUpUncertainty = uncertaintyVoltage(voltageUp, 1100)
+
+upwardsVelocities = np.array([])
+upwardsVelocityUncertainties = np.array([])
+terminalVelocities = np.array([])
+terminalVelocityUncertainties = np.array([])
+
+for position in samples:
+    # Calculating upwards velocity and its uncertainty
+    upwardsTime = np.linspace(
+        0.1, 0.1 * len(position[0]), len(position[0]), endpoint=True
+    )
+    (upwardsVelocity, _), pcovUp = curve_fit(linear, upwardsTime, position[0])
+    upwardsVelocityUncertainty, _ = np.sqrt(np.diag(pcovUp))
+    upwardsVelocities = np.append(upwardsVelocities, upwardsVelocity)
+    upwardsVelocityUncertainties = np.append(
+        upwardsVelocityUncertainties, upwardsVelocityUncertainty
+    )
+
+    # Calculating terminal velocity and its uncertainty
+    terminalTime = np.linspace(
+        0.1, 0.1 * len(position[1]), len(position[1]), endpoint=True
+    )
+    (terminalVelocity, _), pcovTerminal = curve_fit(
+        linear, terminalTime, position[1]
+    )
+    terminalVelocityUncertainty, _ = np.sqrt(np.diag(pcovTerminal))
+    terminalVelocities = np.append(terminalVelocities, terminalVelocity)
+    terminalVelocityUncertainties = np.append(
+        terminalVelocityUncertainties, terminalVelocityUncertainty
+    )
+
+measuredChargesOne = chargeMethodOne(abs(terminalVelocities), stoppingVoltage)
+measuredChargesTwo = chargeMethodTwo(
+    terminalVelocities, upwardsVelocities, voltageUp
+)
+
+# Finding uncertainty in individual elementary charge measurements
+measuredChargesOneUncertainty = uncertaintyChargeMethodOne(
+    terminalVelocities,
+    stoppingVoltage,
+    terminalVelocityUncertainties,
+    stoppingVoltageUncertainty,
+)
+measuredChargesTwoUncertainty = uncertaintyChargeMethodTwo(
+    terminalVelocities,
+    upwardsVelocities,
+    voltageUp,
+    terminalVelocityUncertainties,
+    upwardsVelocityUncertainties,
+    voltageUpUncertainty,
+)
+
+scaledMethodOne = np.rint((measuredChargesOne * 10 ** 21).astype(float)).astype(
+    int
+)
+scaledMethodTwo = np.rint((measuredChargesTwo * 10 ** 21).astype(float)).astype(
+    int
+)
+
+# Adding +/- half of least significant digit to error to account for rounding
+# error and making sure every charge has 3 sig figs
+for i in range(len(scaledMethodOne)):
+    nonSigDigits = len(str(scaledMethodOne[i])) - 3
+    if nonSigDigits != 0:
+        scaledMethodOne[i] = round(scaledMethodOne[i], -nonSigDigits)
+    measuredChargesOneUncertainty[i] = (
+        5 * 10 ** -(20 - nonSigDigits) + measuredChargesOneUncertainty[i]
+    )
+for i in range(len(scaledMethodTwo)):
+    nonSigDigits = len(str(scaledMethodTwo[i])) - 3
+    if nonSigDigits != 0:
+        scaledMethodTwo[i] = round(scaledMethodTwo[i], -nonSigDigits)
+    measuredChargesTwoUncertainty[i] = (
+        5 * 10 ** -(20 - nonSigDigits) + measuredChargesTwoUncertainty[i]
+    )
+
+print(f"GCF for method one: {np.gcd.reduce(scaledMethodOne) / (10**21)}C")
+print(f"GCF for method two: {np.gcd.reduce(scaledMethodTwo) / (10**21)}C")
+print(f"Method One Avg:{np.average(measuredChargesOneUncertainty)}")
+print(f"Method One Max: {max(measuredChargesOneUncertainty)}")
+print(f"Method Two Avg:{np.average(measuredChargesTwoUncertainty)}")
+print(f"Method Two Max: {max(measuredChargesTwoUncertainty)}")
+
